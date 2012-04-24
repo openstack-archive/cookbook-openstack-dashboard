adb2ff81710e7190a46b4001982fb5ec36963d31
Joseph W. Breu <breu@breu.org>
2012-04-24 08:36:41 -0500

    update debugging

--- recipes/server.rb
+++ recipes/server.rb
@@ -55,13 +55,13 @@ else
   # Lookup keystone api ip address
   keystone, start, arbitary_value = Chef::Search::Query.new.search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
   if keystone.length > 0
-    Chef::Log.info("keystone: using search")
+    Chef::Log.info("horizon/keystone: using search")
     keystone_api_ip = keystone[0]['keystone']['api_ipaddress']
     keystone_service_port = keystone[0]['keystone']['service_port']
     keystone_admin_port = keystone[0]['keystone']['admin_port']
     keystone_admin_token = keystone[0]['keystone']['admin_token']
   else
-    Chef::Log.info("keystone: NOT using search")
+    Chef::Log.info("horizon/keystone: NOT using search")
     keystone_api_ip = node['keystone']['api_ipaddress']
     keystone_service_port = node['keystone']['service_port']
     keystone_admin_port = node['keystone']['admin_port']
