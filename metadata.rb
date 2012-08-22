2d829d12feb776350c101ece0ff3d1b2ffd4c50f
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-08-22 10:01:42 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Rackspace US, Inc."
 license          "Apache 2.0"
 description      "Installs/Configures horizon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.6"
+version          "1.0.7"
 
 %w{ ubuntu }.each do |os|
   supports os
