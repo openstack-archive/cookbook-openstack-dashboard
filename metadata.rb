857c77afceb859323441f791fdd028393522ec64
John Dewey <john@dewey.ws>
2013-05-03 12:48:38 -0700

    minor supports syntax

--- metadata.rb
+++ metadata.rb
@@ -9,10 +9,9 @@ version          "2012.2.0"
 recipe           "horizon::db", "Configures database for use with Horizon"
 recipe           "horizon::server", "Sets up the Horizon dashboard within an Apache `mod_wsgi` container."
 
-supports         "centos"
-supports         "fedora"
-supports         "redhat"
-supports         "ubuntu"
+%w{ ubuntu fedora redhat centos }.each do |os|
+  supports os
+end
 
 depends          "apache2"
 depends          "database"
