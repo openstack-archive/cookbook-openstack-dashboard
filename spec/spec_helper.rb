a7d63d11e99e95d7ae704114319df80a76eee3b3
Ionuț Arțăriși <iartarisi@suse.cz>
2013-05-27 17:49:09 +0200

    add openSUSE server spec tests
    - updated to fauxhai 1.1.0 which has platform support for opensuse
    - updated to chefspec 1.2.0 which allows a newer fauxhai version
    
    Change-Id: Ibf1682935e6e1804f97c1b2f992ac99d67a2ef4d

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,17 @@ require "chefspec"
 
 ::LOG_LEVEL = :fatal
 ::REDHAT_OPTS = {
-    :platform  => "redhat",
-    :log_level => ::LOG_LEVEL
+  :platform  => "redhat",
+  :version => "6.3",
+  :log_level => ::LOG_LEVEL
 }
 ::UBUNTU_OPTS = {
-    :platform  => "ubuntu",
-    :version   => "12.04",
-    :log_level => ::LOG_LEVEL
+  :platform  => "ubuntu",
+  :version   => "12.04",
+  :log_level => ::LOG_LEVEL
+}
+::OPENSUSE_OPTS = {
+  :platform => "opensuse",
+  :version => "12.3",
+  :log_level => ::LOG_LEVEL
 }
