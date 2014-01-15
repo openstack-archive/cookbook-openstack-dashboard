87e9ccdf5c0680b110bcd2f943b01a40bbbd7800
Christopher H. Laco <chris.laco@rackspace.com>
2014-01-15 13:53:21 -0600

    Update recipes to be rubocop compliant
    - Add rubocop.yml in recipes/
    - Add recipes rubocop test to Strainerfile
    - Update recipes to comply with rubocop
    
    Change-Id: I2da0aa37596a628f56b908464b696b405fe6aa4e
    Addresses: blueprint rubocop-for-dashboard

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 #
 # Cookbook Name:: openstack-dashboard
 # Recipe:: server
@@ -18,9 +19,9 @@
 # limitations under the License.
 #
 
-require "uri"
+require 'uri'
 
-class ::Chef::Recipe
+class ::Chef::Recipe # rubocop:disable Documentation
   include ::Openstack
 end
 
@@ -30,169 +31,169 @@ end
 #             and it should simply be a restorecon on the configuration file(s) and not
 #             change the selinux mode
 #
-execute "set-selinux-permissive" do
-  command "/sbin/setenforce Permissive"
+execute 'set-selinux-permissive' do
+  command '/sbin/setenforce Permissive'
   action :run
 
   only_if "[ ! -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*enforcing') -eq 1 ]"
 end
 
-platform_options = node["openstack"]["dashboard"]["platform"]
+platform_options = node['openstack']['dashboard']['platform']
 
-include_recipe "apache2"
-include_recipe "apache2::mod_wsgi"
-include_recipe "apache2::mod_rewrite"
-include_recipe "apache2::mod_ssl"
+include_recipe 'apache2'
+include_recipe 'apache2::mod_wsgi'
+include_recipe 'apache2::mod_rewrite'
+include_recipe 'apache2::mod_ssl'
 
 #
 # Workaround to re-enable selinux after installing apache on a fedora machine that has
 # selinux enabled and is currently permissive and the configuration set to enforcing.
 # TODO(breu): get the other one working and this won't be necessary
 #
-execute "set-selinux-enforcing" do
-  command "/sbin/setenforce Enforcing ; restorecon -R /etc/httpd"
+execute 'set-selinux-enforcing' do
+  command '/sbin/setenforce Enforcing ; restorecon -R /etc/httpd'
   action :run
 
   only_if "[ -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*permissive') -eq 1 ] && [ $(/sbin/sestatus | grep -c '^Mode from config file:.*enforcing') -eq 1 ]"
 end
 
-identity_admin_endpoint = endpoint "identity-admin"
+identity_admin_endpoint = endpoint 'identity-admin'
 auth_admin_uri = ::URI.decode identity_admin_endpoint.to_s
-identity_endpoint = endpoint "identity-api"
+identity_endpoint = endpoint 'identity-api'
 auth_uri = ::URI.decode identity_endpoint.to_s
 
-db_pass = db_password "horizon"
-db_info = db "dashboard"
+db_pass = db_password 'horizon'
+db_info = db 'dashboard'
 
 python_packages = platform_options["#{db_info['db_type']}_python_packages"]
-(platform_options["horizon_packages"] + python_packages).each do |pkg|
+(platform_options['horizon_packages'] + python_packages).each do |pkg|
   package pkg do
     action :upgrade
-    options platform_options["package_overrides"]
+    options platform_options['package_overrides']
   end
 end
 
-if node["openstack"]["dashboard"]["session_backend"] == "memcached"
-  platform_options["memcache_python_packages"].each do |pkg|
+if node['openstack']['dashboard']['session_backend'] == 'memcached'
+  platform_options['memcache_python_packages'].each do |pkg|
     package pkg
   end
 end
 
 memcached = memcached_servers
 
-template node["openstack"]["dashboard"]["local_settings_path"] do
-  source "local_settings.py.erb"
-  owner  "root"
-  group  "root"
+template node['openstack']['dashboard']['local_settings_path'] do
+  source 'local_settings.py.erb'
+  owner  'root'
+  group  'root'
   mode   00644
 
   variables(
-    :db_pass => db_pass,
-    :db_info => db_info,
-    :auth_uri => auth_uri,
-    :auth_admin_uri => auth_admin_uri,
-    :memcached_servers => memcached
+    db_pass: db_pass,
+    db_info: db_info,
+    auth_uri: auth_uri,
+    auth_admin_uri: auth_admin_uri,
+    memcached_servers: memcached
   )
 
-  notifies :restart, "service[apache2]"
+  notifies :restart, 'service[apache2]'
 end
 
 # FIXME: this shouldn't run every chef run
-execute "openstack-dashboard syncdb" do
-  cwd "/usr/share/openstack-dashboard"
-  environment ({'PYTHONPATH' => '/etc/openstack-dashboard:/usr/share/openstack-dashboard:$PYTHONPATH'})
-  command "python manage.py syncdb --noinput"
+execute 'openstack-dashboard syncdb' do
+  cwd '/usr/share/openstack-dashboard'
+  environment 'PYTHONPATH' => '/etc/openstack-dashboard:/usr/share/openstack-dashboard:$PYTHONPATH'
+  command 'python manage.py syncdb --noinput'
   action :run
   # not_if "/usr/bin/mysql -u root -e 'describe #{node["dash"]["db"]}.django_content_type'"
 end
 
 cookbook_file "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/certs/#{node["openstack"]["dashboard"]["ssl"]["cert"]}" do
-  source "horizon.pem"
+  source 'horizon.pem'
   mode   00644
-  owner  "root"
-  group  "root"
+  owner  'root'
+  group  'root'
 
-  notifies :run, "execute[restore-selinux-context]", :immediately
+  notifies :run, 'execute[restore-selinux-context]', :immediately
 end
 
-case node["platform"]
-when "ubuntu","debian"
-  grp = "ssl-cert"
+case node['platform']
+when 'ubuntu', 'debian'
+  grp = 'ssl-cert'
 else
-  grp = "root"
+  grp = 'root'
 end
 
 cookbook_file "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/private/#{node["openstack"]["dashboard"]["ssl"]["key"]}" do
-  source "horizon.key"
+  source 'horizon.key'
   mode   00640
-  owner  "root"
+  owner  'root'
   group  grp # Don't know about fedora
 
-  notifies :run, "execute[restore-selinux-context]", :immediately
+  notifies :run, 'execute[restore-selinux-context]', :immediately
 end
 
 # stop apache bitching
 directory "#{node["openstack"]["dashboard"]["dash_path"]}/.blackhole" do
-  owner "root"
+  owner 'root'
   action :create
 end
 
-template node["openstack"]["dashboard"]["apache"]["sites-path"] do
-  source "dash-site.erb"
-  owner  "root"
-  group  "root"
+template node['openstack']['dashboard']['apache']['sites-path'] do
+  source 'dash-site.erb'
+  owner  'root'
+  group  'root'
   mode   00644
 
   variables(
-    :ssl_cert_file => "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/certs/#{node["openstack"]["dashboard"]["ssl"]["cert"]}",
-    :ssl_key_file => "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/private/#{node["openstack"]["dashboard"]["ssl"]["key"]}"
+    ssl_cert_file: "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/certs/#{node["openstack"]["dashboard"]["ssl"]["cert"]}",
+    ssl_key_file: "#{node["openstack"]["dashboard"]["ssl"]["dir"]}/private/#{node["openstack"]["dashboard"]["ssl"]["key"]}"
   )
 
-  notifies :run, "execute[restore-selinux-context]", :immediately
+  notifies :run, 'execute[restore-selinux-context]', :immediately
 end
 
 file "#{node["apache"]["dir"]}/conf.d/openstack-dashboard.conf" do
   action :delete
   backup false
 
-  only_if { platform?("fedora", "redhat", "centos") } # :pragma-foodcritic: ~FC024 - won't fix this
+  only_if { platform?('fedora', 'redhat', 'centos') } # :pragma-foodcritic: ~FC024 - won't fix this
 end
 
 # ubuntu includes their own branding - we need to delete this until ubuntu makes this a
 # configurable paramter
-package "openstack-dashboard-ubuntu-theme" do
+package 'openstack-dashboard-ubuntu-theme' do
   action :purge
 
-  only_if { platform?("ubuntu")}
+  only_if { platform?('ubuntu') }
 end
 
 # The `apache_site` provided by the apache2 cookbook
 # is not an LWRP. Guards do not apply to definitions.
 # http://tickets.opscode.com/browse/CHEF-778
-if platform?("debian","ubuntu") then
-  apache_site "000-default" do
+if platform?('debian', 'ubuntu')
+  apache_site '000-default' do
     enable false
   end
-elsif platform?("fedora") then
-  apache_site "default" do
+elsif platform?('fedora') then
+  apache_site 'default' do
     enable false
 
-    notifies :run, "execute[restore-selinux-context]", :immediately
+    notifies :run, 'execute[restore-selinux-context]', :immediately
   end
 end
 
-apache_site "openstack-dashboard" do
+apache_site 'openstack-dashboard' do
   enable true
 
-  notifies :run, "execute[restore-selinux-context]", :immediately
-  notifies :reload, "service[apache2]", :immediately
+  notifies :run, 'execute[restore-selinux-context]', :immediately
+  notifies :reload, 'service[apache2]', :immediately
 end
 
-execute "restore-selinux-context" do
-  command "restorecon -Rv /etc/httpd /etc/pki; chcon -R -t httpd_sys_content_t /usr/share/openstack-dashboard || :"
+execute 'restore-selinux-context' do
+  command 'restorecon -Rv /etc/httpd /etc/pki; chcon -R -t httpd_sys_content_t /usr/share/openstack-dashboard || :'
   action :nothing
 
-  only_if { platform?("fedora") }
+  only_if { platform?('fedora') }
 end
 
 # TODO(shep)
