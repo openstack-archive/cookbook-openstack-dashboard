f41e92dbbf209d6c04cabeb6cd84f4204bc0de6c
Darren Birkett <darren.birkett@gmail.com>
2014-01-23 11:22:15 +0000

    refactor chefspec tests to be faster and more efficient
    Change-Id: I1a5f22b719db453d55d7f080cf0e1b102e11bacf
    Implements: blueprint refactor-spec-files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,42 +2,64 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-::LOG_LEVEL = :fatal
-::FEDORA_OPTS = {
+LOG_LEVEL = :fatal
+FEDORA_OPTS = {
   platform: 'fedora',
   version: '18',
-  log_level: ::LOG_LEVEL
+  log_level: LOG_LEVEL
 }
-::REDHAT_OPTS = {
+REDHAT_OPTS = {
   platform: 'redhat',
-  version: '6.3',
-  log_level: ::LOG_LEVEL
+  version: '6.5',
+  log_level: LOG_LEVEL
 }
-::UBUNTU_OPTS = {
+UBUNTU_OPTS = {
   platform: 'ubuntu',
   version: '12.04',
-  log_level: ::LOG_LEVEL
+  log_level: LOG_LEVEL
 }
-::OPENSUSE_OPTS = {
+OPENSUSE_OPTS = {
   platform: 'opensuse',
   version: '12.3',
-  log_level: ::LOG_LEVEL
+  log_level: LOG_LEVEL
 }
 
-def dashboard_stubs
-  ::Chef::Recipe.any_instance.stub(:memcached_servers)
+shared_context 'dashboard_stubs' do
+  before do
+    Chef::Recipe.any_instance.stub(:memcached_servers)
     .and_return ['hostA:port', 'hostB:port']
-  ::Chef::Recipe.any_instance.stub(:get_password)
+    Chef::Recipe.any_instance.stub(:get_password)
     .with('db', 'horizon')
-    .and_return 'test-passes'
+    .and_return('test-passes')
+  end
 end
 
-def redhat_stubs
-  stub_command("[ ! -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*enforcing') -eq 1 ]").and_return(true)
-  stub_command("[ -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*permissive') -eq 1 ] && [ $(/sbin/sestatus | grep -c '^Mode from config file:.*enforcing') -eq 1 ]").and_return(true)
+shared_context 'redhat_stubs' do
+  before do
+    stub_command("[ ! -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*enforcing') -eq 1 ]").and_return(true)
+    stub_command("[ -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*permissive') -eq 1 ] && [ $(/sbin/sestatus | grep -c '^Mode from config file:.*enforcing') -eq 1 ]").and_return(true)
+  end
 end
 
-def non_redhat_stubs
-  stub_command("[ ! -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*enforcing') -eq 1 ]").and_return(false)
-  stub_command("[ -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*permissive') -eq 1 ] && [ $(/sbin/sestatus | grep -c '^Mode from config file:.*enforcing') -eq 1 ]").and_return(false)
+shared_context 'non_redhat_stubs' do
+  before do
+    stub_command("[ ! -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*enforcing') -eq 1 ]").and_return(false)
+    stub_command("[ -e /etc/httpd/conf/httpd.conf ] && [ -e /etc/redhat-release ] && [ $(/sbin/sestatus | grep -c '^Current mode:.*permissive') -eq 1 ] && [ $(/sbin/sestatus | grep -c '^Mode from config file:.*enforcing') -eq 1 ]").and_return(false)
+  end
+end
+
+shared_context 'postgresql_backend' do
+  before do
+    Chef::Recipe.any_instance.stub(:db)
+    .with('dashboard')
+    .and_return('service_type' => 'postgresql', 'db_name' => 'flying_elephant')
+  end
+end
+
+shared_context 'mysql_backend' do
+  before do
+    Chef::Recipe.any_instance.stub(:db)
+    .with('dashboard')
+    .and_return('service_type' => 'mysql', 'db_name' => 'flying_dolphin')
+  end
 end
