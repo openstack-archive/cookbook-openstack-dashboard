e515bf84e17d335d54736f073f5c23cd0240a17d
Jan Klare <j.klare@cloudbau.de>
2016-05-31 10:29:34 +0200

    initial commit for the newton development cycle
    Change-Id: I6029c66f54f9e0491d5f1b327665e598b0400233

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'Installs/Configures the OpenStack Dashboard (Horizon)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '13.0.0'
+version '14.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'apache2', '~> 3.1'
-depends 'openstack-common', '>= 13.0.0'
-depends 'openstack-identity', '>= 13.0.0'
+depends 'openstack-common', '>= 14.0.0'
+depends 'openstack-identity', '>= 14.0.0'
