a90290cb0730e38c3a633348d3693fa3492ea787
Jan Klare <j.klare@cloudbau.de>
2017-02-23 17:06:14 +0100

    starting ocata development patch
    Change-Id: I1bf3ea7daa127ebf4545e6c9ea809ef4351efc4b

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,13 @@ source_url 'https://github.com/openstack/cookbook-openstack-dashboard' if respon
 license 'Apache 2.0'
 description 'Installs/Configures the OpenStack Dashboard (Horizon)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '14.0.0'
+version '15.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'apache2', '~> 3.2'
-depends 'openstack-common', '>= 14.0.0'
-depends 'openstack-identity', '>= 14.0.0'
+depends 'openstack-common', '>= 15.0.0'
+depends 'openstack-identity', '>= 15.0.0'
 depends 'poise-python', '~> 1.5.1'
